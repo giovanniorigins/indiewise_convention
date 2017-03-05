dbcbc1581ef5ae46535de5bde1d02c34bae51602
Jerez Bain <jerezb31@gmail.com>
2017-03-04 19:37:20 -0500

    minor changes

--- index.php
+++ index.php
@@ -15,26 +15,26 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<!-- Favicon -->
-		<link rel="apple-touch-icon" sizes="57x57" href="../apple-touch-icon-57x57.png">
-		<link rel="apple-touch-icon" sizes="60x60" href="../apple-touch-icon-60x60.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="../apple-touch-icon-72x72.png">
-		<link rel="apple-touch-icon" sizes="76x76" href="../apple-touch-icon-76x76.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="../apple-touch-icon-114x114.png">
-		<link rel="apple-touch-icon" sizes="120x120" href="../apple-touch-icon-120x120.png">
-		<link rel="apple-touch-icon" sizes="144x144" href="../apple-touch-icon-144x144.png">
-		<link rel="apple-touch-icon" sizes="152x152" href="../apple-touch-icon-152x152.png">
-		<link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon-180x180.png">
-		<link rel="icon" type="image/png" href="../favicon-32x32.png" sizes="32x32">
-		<link rel="icon" type="image/png" href="../favicon-194x194.png" sizes="194x194">
-		<link rel="icon" type="image/png" href="../favicon-96x96.png" sizes="96x96">
-		<link rel="icon" type="image/png" href="../android-chrome-192x192.png" sizes="192x192">
-		<link rel="icon" type="image/png" href="../favicon-16x16.png" sizes="16x16">
-		<link rel="manifest" href="../manifest.json">
-		<link rel="mask-icon" href="../safari-pinned-tab.svg" color="#5bbad5">
+		<link rel="apple-touch-icon" sizes="57x57" href="./apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="./apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="./apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="./apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="./apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="./apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="./apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="./apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="./favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="./favicon-194x194.png" sizes="194x194">
+		<link rel="icon" type="image/png" href="./favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="./android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="./favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="./manifest.json">
+		<link rel="mask-icon" href="./safari-pinned-tab.svg" color="#5bbad5">
 		<meta name="msapplication-TileColor" content="#2b5797">
-		<meta name="msapplication-TileImage" content="../mstile-144x144.png">
+		<meta name="msapplication-TileImage" content="./mstile-144x144.png">
 		<meta name="theme-color" content="#ffffff">
-		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="shortcut icon" href="./favicon.ico">
 
 		<!-- Web Fonts -->
 		<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
@@ -261,6 +261,8 @@
 													<!-- mega-menu start -->
 													<li class="active">
 														<a href="/">Home</a>
+													</li>
+													<li>
 														<a href="#">Register Soon</a>
 													</li>
 												</ul>
@@ -304,7 +306,7 @@
 								<li class="text-center" data-transition="fade" data-slotamount="1" data-masterspeed="500" data-saveperformance="off">
 
 									<!-- main image -->
-									<img src="../media/img.jpg" alt="slidebg1" data-bgposition="center bottom" data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+									<img src="./media/img.jpg" alt="slidebg1" data-bgposition="center bottom" data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
 
 									<!-- Transparent Background -->
 									<div class="tp-caption dark-translucent-bg"
@@ -351,7 +353,7 @@
 									     data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
 									     data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
 									     data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;">
-										<span class="text-rotator" data-rotator-animation-effect="fadeIn">Boost your online presence, Refine your online presence, Improve your online presence, Beautify your online presence</span>
+										<span class="text-rotator" data-rotator-animation-effect="fadeIn">You be the Judge</span>
 									</div>
 
 									<!-- LAYER NR. 4 -->
