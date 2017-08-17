85b08181779c880c1f2e5e351fa389b8ef36f8c0
Jerez Bain <jerezb31@gmail.com>
2017-08-17 05:51:24 -0400

    update nav titles - awaiting files

--- partials/nav.php
+++ partials/nav.php
@@ -58,7 +58,7 @@
                                             <a href="https://www.eventbrite.com/e/grand-indiewise-convention-package-deals-single-event-passes-tickets-32982861627#tickets" target="_blank">Register</a>
                                         </li>
                                         <li>
-                                            <a href="https://cdn.filestackcontent.com/kNlCfTSqTzG9m6K7ZaJX?cache=true" target="_blank">Schedule</a>
+                                            <a href="" target="_blank">2017 Screenings</a>
                                         </li>
                                         <li>
                                             <a href="https://cdn.filestackcontent.com/0dxKxiSTUOnB4Tm8GtNU?cache=true" target="_blank">Programme 2017</a>
